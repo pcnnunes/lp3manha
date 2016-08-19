068882840813b19e50d0bed8b1607dc9c361d2fd
pcnnunes <p.cn@hotmail.com>
2016-08-19 09:13:34 -0300

    Classe Usuario adicionada

--- TodoWeb/Global.asax.cs
+++ TodoWeb/Global.asax.cs
@@ -15,6 +15,7 @@ namespace TodoWeb
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
